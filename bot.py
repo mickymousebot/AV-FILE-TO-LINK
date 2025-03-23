91ef2346161af87afa9d81ce0e2a2b334d5fd544
Micky Mouse <techsashif+eufya@gmail.com>
2025-03-23 23:36:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,23 @@
-import os, sys, glob, pytz, asyncio, logging, importlib
+import os
+import sys
+import glob
+import pytz
+import asyncio
+import logging
+import importlib
 from pathlib import Path
 from pyrogram import idle
+from typing import Union, Optional, AsyncGenerator
+from datetime import date, datetime
+from aiohttp import web
+from web import web_server
+from web.server import Webavbot
+from utils import temp, ping_server
+from web.server.clients import initialize_clients
+from info import *
+from Script import script
 
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
-
+# Logging configuration
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -13,31 +25,18 @@ logging.basicConfig(
 logging.getLogger("aiohttp").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
- 
-from info import *
-from typing import Union, Optional, AsyncGenerator
-from Script import script 
-from datetime import date, datetime 
-from aiohttp import web
-from web import web_server
-from web.server import Webavbot
-from utils import temp, ping_server
-from web.server.clients import initialize_clients
 
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
-
+# Plugin loading
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
-Webavbot.start()
-loop = asyncio.get_event_loop()
 
 async def start():
     print('\n')
-    print('Initalizing Your Bot')
+    print('Initializing Your Bot')
     bot_info = await Webavbot.get_me()
     await initialize_clients()
+
+    # Load plugins
     for name in files:
         with open(name) as a:
             patt = Path(a.name)
@@ -50,36 +49,38 @@ async def start():
             sys.modules["plugins." + plugin_name] = load
             print("Imported => " + plugin_name)
 
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
-    
+    # Ping server if on Heroku
     if ON_HEROKU:
         asyncio.create_task(ping_server())
+
+    # Set bot information
     me = await Webavbot.get_me()
     temp.BOT = Webavbot
     temp.ME = me.id
     temp.U_NAME = me.username
     temp.B_NAME = me.first_name
+
+    # Send restart message
     tz = pytz.timezone('Asia/Kolkata')
     today = date.today()
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
     await Webavbot.send_message(LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
     await Webavbot.send_message(ADMINS[0], text='<b>ʙᴏᴛ ʀᴇsᴛᴀʀᴛᴇᴅ !!</b>')
-    app = web.AppRunner(await web_server())
-    await app.setup()
-    bind_address = "0.0.0.0"
-    await web.TCPSite(app, bind_address, PORT).start()
-    await idle()
 
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
+    # Start aiohttp web server
+    app = web.Application()
+    app.add_routes([web.get("/", lambda request: web.Response(text="Bot is running!"))])
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", PORT)
+    await site.start()
+
+    # Keep the bot running
+    await idle()
 
 if __name__ == '__main__':
     try:
-        loop.run_until_complete(start())
+        asyncio.run(start())  # Use asyncio.run() instead of get_event_loop()
     except KeyboardInterrupt:
         logging.info('----------------------- Service Stopped -----------------------')
-
