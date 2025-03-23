b2318666b57630009c2b49bef61831aa5b7157a2
Micky Mouse <techsashif+eufya@gmail.com>
2025-03-23 21:02:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,9 @@
-import os
-import sys
-import glob
-import pytz
-import asyncio
-import logging
-import importlib
+import os, sys, glob, pytz, asyncio, logging, importlib
 from pathlib import Path
 from pyrogram import idle
 
-# Dont Remove My Credit @AV_BOTz_UPDATE
-# This Repo Is By @BOT_OWNER26
+#Dont Remove My Credit @AV_BOTz_UPDATE 
+#This Repo Is By @BOT_OWNER26 
 # For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
 
 logging.basicConfig(
@@ -19,32 +13,29 @@ logging.basicConfig(
 logging.getLogger("aiohttp").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
-
+ 
 from info import *
 from typing import Union, Optional, AsyncGenerator
-from Script import script
-from datetime import date, datetime
+from Script import script 
+from datetime import date, datetime 
 from aiohttp import web
 from web import web_server
 from web.server import Webavbot
 from utils import temp, ping_server
 from web.server.clients import initialize_clients
 
-# Dont Remove My Credit @AV_BOTz_UPDATE
-# This Repo Is By @BOT_OWNER26
+#Dont Remove My Credit @AV_BOTz_UPDATE 
+#This Repo Is By @BOT_OWNER26 
 # For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
 
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
 Webavbot.start()
-
-# Fix for DeprecationWarning: Use asyncio.new_event_loop() instead of asyncio.get_event_loop()
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
+loop = asyncio.get_event_loop()
 
 async def start():
     print('\n')
-    print('Initializing Your Bot')
+    print('Initalizing Your Bot')
     bot_info = await Webavbot.get_me()
     await initialize_clients()
     for name in files:
@@ -59,6 +50,10 @@ async def start():
             sys.modules["plugins." + plugin_name] = load
             print("Imported => " + plugin_name)
 
+#Dont Remove My Credit @AV_BOTz_UPDATE 
+#This Repo Is By @BOT_OWNER26 
+# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
+    
     if ON_HEROKU:
         asyncio.create_task(ping_server())
     me = await Webavbot.get_me()
@@ -72,16 +67,14 @@ async def start():
     time = now.strftime("%H:%M:%S %p")
     await Webavbot.send_message(LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
     await Webavbot.send_message(ADMINS[0], text='<b>ʙᴏᴛ ʀᴇsᴛᴀʀᴛᴇᴅ !!</b>')
-
-    # Start aiohttp web server
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
     await web.TCPSite(app, bind_address, PORT).start()
     await idle()
 
-# Dont Remove My Credit @AV_BOTz_UPDATE
-# This Repo Is By @BOT_OWNER26
+#Dont Remove My Credit @AV_BOTz_UPDATE 
+#This Repo Is By @BOT_OWNER26 
 # For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
 
 if __name__ == '__main__':
@@ -89,3 +82,4 @@ if __name__ == '__main__':
         loop.run_until_complete(start())
     except KeyboardInterrupt:
         logging.info('----------------------- Service Stopped -----------------------')
+
